5d9c285d20d386e8f5bae3ca467a4fe83af3713f
Kush Sharma <3647166+kushsharma@users.noreply.github.com>
2021-06-04 01:44:40 +0530

    Brew formula update for optimus-plugins version v0.0.2

--- optimus-plugins.rb
+++ optimus-plugins.rb
@@ -5,33 +5,33 @@
 class OptimusPlugins < Formula
   desc "Optimus Near earth orbit tracker"
   homepage ""
-  version "0.0.1"
+  version "0.0.2"
   license "Apache 2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kushsharma/optimus-plugins/releases/download/v0.0.1/optimus-plugins_0.0.1_darwin_amd64.tar.gz"
-      sha256 "eea0c7a9d00d76c2a320054c19f63f3980006d8d0dd38919c037ffc3688e1959"
+      url "https://github.com/kushsharma/optimus-plugins/releases/download/v0.0.2/optimus-plugins_0.0.2_darwin_amd64.tar.gz"
+      sha256 "2d215707e871b02939f7d109ea6385a72ff50f4beed513f1617cd3e51c63af44"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kushsharma/optimus-plugins/releases/download/v0.0.1/optimus-plugins_0.0.1_darwin_arm64.tar.gz"
-      sha256 "d94f32a92431e248e8db5c8810a1954a0bb2bccecb260cbcfce36b3303babd30"
+      url "https://github.com/kushsharma/optimus-plugins/releases/download/v0.0.2/optimus-plugins_0.0.2_darwin_arm64.tar.gz"
+      sha256 "75aac80c45f8a2af677691934c175313bad4205ecb7c98fe5f1a4ae45e6e1c2a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/kushsharma/optimus-plugins/releases/download/v0.0.1/optimus-plugins_0.0.1_linux_amd64.tar.gz"
-      sha256 "42f60d73b89b4d500189c4cbba03a60e0683c3a886454e2283f4882fee8f53f9"
+      url "https://github.com/kushsharma/optimus-plugins/releases/download/v0.0.2/optimus-plugins_0.0.2_linux_amd64.tar.gz"
+      sha256 "0b185d6777c3d3a6a1c3f6d5b0aff40ace171eceae954caa5acd9b96d6f1468f"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kushsharma/optimus-plugins/releases/download/v0.0.1/optimus-plugins_0.0.1_linux_arm64.tar.gz"
-      sha256 "b9a2a08bebed8b840bce17cfee83053d9759e514e7debbe8fbb6a51736a906a9"
+      url "https://github.com/kushsharma/optimus-plugins/releases/download/v0.0.2/optimus-plugins_0.0.2_linux_arm64.tar.gz"
+      sha256 "6d09392c81b68bf82ed94d0b841bb9f272acee43a439f06622cc44522ede08b2"
     end
   end
 
   def install
-    bin.install "optimus-plugins"
+    bin.install Dir["optimus-*"]
   end
 end
